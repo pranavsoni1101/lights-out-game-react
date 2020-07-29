795095a4bcddc70737251b9a13975462006873db
pranavsoni1101 <55845982+pranavsoni1101@users.noreply.github.com>
2020-07-29 20:17:51 +0530

    Updates

---
+++ precache-manifest.d338eb27daf9440d755012c223555a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "582b979f0a4e5c62b9ac9872593a1d0e",
+    "url": "/lights-out-game-react/index.html"
+  },
+  {
+    "revision": "faee9977801442749ff8",
+    "url": "/lights-out-game-react/static/css/main.0e3a8e70.chunk.css"
+  },
+  {
+    "revision": "d026c9cc2ab35ac15b9c",
+    "url": "/lights-out-game-react/static/js/2.acdcdc98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lights-out-game-react/static/js/2.acdcdc98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faee9977801442749ff8",
+    "url": "/lights-out-game-react/static/js/main.c3b055d9.chunk.js"
+  },
+  {
+    "revision": "5595811b842116441fdc",
+    "url": "/lights-out-game-react/static/js/runtime-main.542bccdc.js"
+  }
+]);
